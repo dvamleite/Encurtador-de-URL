660d55720fde645e359436c4618ae71320186b12
Dvam leite <dvanlds@gmail.com>
2021-11-18 17:03:55 -0300

    Add files via upload

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: `mongodb+srv://dvanlds:RfSbauGvx9GvDCyA@cluster0.z1vxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		`mongodb+srv://dvanlds:RfSbauGvx9GvDCyA@cluster0.z1vxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+}
