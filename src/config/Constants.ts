62cc5095340aa8bc45d6aa0dd1514e5b08061e32
Dvam leite <dvanlds@gmail.com>
2021-11-18 17:19:02 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		`mongodb+srv://dvanlds:RfSbauGvx9GvDCyA@cluster0.z1vxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+		`mongodb+srv://dvanlds:<Sua Senha Do MongoDB>@cluster0.z1vxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
 }
